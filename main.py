91fc283aaef7df5b159cfdb04afc130bf2436a96
tangled-beep <74329470+tangled-beep@users.noreply.github.com>
2020-11-14 10:52:18 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import sched, time
+s = sched.scheduler(time.time, time.sleep)
+
+
+def job_search(sc):
+    print("Searching...")
+    #define job source URL
+    URL = 'https://jobs.lever.co/izotope'
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    #define keyword to look for
+    results = soup.body.findAll(text=re.compile('Engineer'), limit=10)
+
+    #defines connection / http error
+
+    print(results)
+    s.enter(5, 1, job_search, (sc, ))
+
+#rate of repeater
+s.enter(5, 1, job_search, (s, ))
+s.run()
